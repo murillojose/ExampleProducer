f9ae6ed87aea5c8e6f3f6bbe761a9640d142459d
Murillo Jose <murillojosema@gmail.com>
2024-02-06 22:50:40 -0300

    adjust tests

---
+++ BemolProducer.Test/Controllers/ProdutoController.test.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using AutoMapper;
+using BemolProducer.Controllers;
+using BemolProducer.Domain;
+using BemolProducer.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Moq;
+using NUnit.Framework;
+
+namespace BemolProducer.Test.Controllers
+{
+    public class ProdutoControllerTest
+    {
+        private ProdutosController _produtosController;
+        private Mock<IProdutoService> _produtoService;
+        private Mock<QueueClient> _queueClient;
+        private Mock<IMapper> _mapper;
+        private Fixture _fixture;
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = new Mock<IMapper>();
+            _queueClient = new Mock<QueueClient>("connectionString", "queueName");
+            _produtoService = new Mock<IProdutoService>();
+            _produtosController = new ProdutosController(_produtoService.Object, _queueClient.Object, _mapper.Object);
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public async Task GetProdutos_ShouldBeSucces()
+        {
+            var expectedProdutos = _fixture.Create<IEnumerable<Produto>>();
+
+            _produtoService.Setup(s => s.GetAllAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(expectedProdutos);
+
+            var result = await _produtosController.GetProdutos();
+
+            var okResult = result as OkObjectResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+        
+    }
+}
---
+++ BemolProducer.Test/Services/Service.test.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture;
+using BemolProducer.Domain;
+using BemolProducer.Domain.Interfaces;
+using BemolProducer.Service;
+using Moq;
+using NUnit.Framework;
+
+namespace BemolProducer.Test
+{
+    public class ServiceTest
+    {
+        private Fixture _fixture;
+        private Mock<IProdutoRepository> _mockedRepository;
+        private ProdutoService _produtoService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+            _mockedRepository = new Mock<IProdutoRepository>();
+            _produtoService = new ProdutoService(_mockedRepository.Object);
+        }
+
+        [Test]
+        public async Task GetAllProducts_Should_Be_Success()
+        {
+            IEnumerable<Produto> produtos = _fixture.Create<IEnumerable<Produto>>();
+            _mockedRepository.Setup(r => r.GetAllAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(produtos);
+
+            var result =  await _produtoService.GetAllAsync(0,0);
+            
+            Assert.IsNotNull(result);
+
+            Assert.IsTrue(produtos.SequenceEqual(result));
+        }
+
+        [Test]
+        public async Task CreateAsync_Should_Be_Success()
+        {
+            Produto produto = _fixture.Create<Produto>();
+
+            await _produtoService.CreateAsync(produto);
+
+            _mockedRepository.Verify(r => r.SaveAsync(produto), Times.Once);
+        }
+
+    }
+}
